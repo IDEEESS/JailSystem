3b9b31611f89a8e7d3732b3de4e29c484122c3e9
IDEEESS <64473138+IDEEESS@users.noreply.github.com>
2023-07-07 11:46:52 +0200

    Setup methods and config.yml

--- src/main/java/fr/ideeess/main/JailSystem.java
+++ src/main/java/fr/ideeess/main/JailSystem.java
@@ -1,19 +1,89 @@
 package fr.ideeess.main;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public class JailSystem extends JavaPlugin {
 
+    public static final String WORLDJAILNAME = new JailSystem().getConfig().getString("setup.worldJailName");
+    public static final String JAILCOMMANDPERMISSION = new JailSystem().getConfig().getString("setup.permissions.jailCommandPermission");
+    public static final String UNJAILCOMMANDPERMISSION = new JailSystem().getConfig().getString("setup.permissions.unjailCommandPermission");
+    public static final String JAILBYPASSPERMISSION = new JailSystem().getConfig().getString("setup.permissions.bypassJail");
+    public static int JAILSPAWNX;
+    public static int JAILSPAWNY;
+    public static int JAILSPAWNZ;
+    public static boolean DEFINEJAILSPAWN = false;
+    public static boolean DEFINEJAILZONE = false;
+    public static int JAILZONEX;
+    public static int JAILZONEY;
+    public static int JAILZONEZ;
+    public static int JAILZONEX1;
+    public static int JAILZONEY1;
+    public static int JAILZONEZ1;
+    public static Location JAILSPAWNLOCATION;
+    public static int JAILEXITSPAWNX;
+    public static int JAILEXITSPAWNY;
+    public static int JAILEXITSPAWNZ;
+    public static boolean DEFINEJAILEXITSPAWN = false;
+    public static Location JAILEXITLOCATION;
+
     @Override
     public void onEnable() {
+
         super.onEnable();
 
         System.out.println("This plugin has been enabled");
 
+        //Config
         this.saveConfig();
         this.saveDefaultConfig();
         getConfig().options().copyDefaults(true);
 
+        // Vérifications
+        if (!Bukkit.getWorlds().contains(WORLDJAILNAME)){
+            this.getServer().getPluginManager().disablePlugin(this);
+            System.out.println("Le monde \""+WORLDJAILNAME+"\" est introuvable . Veuillez modifier le nom du monde dans le fichier config.yml du plugin ou créer le monde.");
+        }
+
+        if (!(getConfig().get("setup.jailSpawnLocation.x") instanceof Integer && getConfig().get("setup.jailSpawnLocation.y") instanceof Integer && getConfig().get("setup.jailSpawnLocation.z") instanceof Integer)){
+            Bukkit.getLogger().severe("Merci de définir le point de spawn dans la prison");
+            DEFINEJAILSPAWN = true;
+        }else {
+            JAILSPAWNX = (int) getConfig().get("setup.jailSpawnLocation.x");
+            JAILSPAWNY = (int) getConfig().get("setup.jailSpawnLocation.y");
+            JAILSPAWNZ = (int) getConfig().get("setup.jailSpawnLocation.z");
+        }
+
+        if (!(getConfig().get("setup.jail.x") instanceof Integer && getConfig().get("setup.jail.y") instanceof Integer && getConfig().get("setup.jail.z") instanceof Integer && getConfig().get("setup.jail.x1") instanceof Integer && getConfig().get("setup.jail.y2") instanceof Integer && getConfig().get("setup.jail.z2") instanceof Integer)){
+            Bukkit.getLogger().severe("Merci de définir la zone de la prison");
+            DEFINEJAILZONE = true;
+        }else{
+            JAILZONEX = (int) getConfig().get("setup.jail.x");
+            JAILZONEY = (int) getConfig().get("setup.jail.y");
+            JAILZONEZ = (int) getConfig().get("setup.jail.z");
+            JAILZONEX1 = (int) getConfig().get("setup.jail.x1");
+            JAILZONEY1 = (int) getConfig().get("setup.jail.y1");
+            JAILZONEZ1 = (int) getConfig().get("setup.jail.z1");
+        }
+
+        if (!DEFINEJAILSPAWN){
+            JAILSPAWNLOCATION = new Location(Bukkit.getWorld(WORLDJAILNAME),JAILSPAWNX , JAILSPAWNY , JAILSPAWNZ);
+        }
+
+        if (!(getConfig().get("setup.jailexit.x") instanceof Integer && getConfig().get("setup.jailexit.y") instanceof Integer && getConfig().get("setup.jailexit.z") instanceof Integer)){
+            Bukkit.getLogger().severe("Merci de définir le point de spawn de la sortie de prison");
+            DEFINEJAILEXITSPAWN = true;
+        }else {
+            JAILEXITSPAWNX = (int) getConfig().get("setup.jailexit.x");
+            JAILEXITSPAWNY = (int) getConfig().get("setup.jailexit.y");
+            JAILEXITSPAWNZ = (int) getConfig().get("setup.jailexit.z");
+        }
+
+        if(!DEFINEJAILEXITSPAWN){
+            JAILEXITLOCATION = new Location(Bukkit.getWorld(WORLDJAILNAME),JAILEXITSPAWNX , JAILEXITSPAWNY , JAILEXITSPAWNZ);
+        }
     }
 
     @Override
--- src/main/java/fr/ideeess/manager/PrisonersManager.java
+++ src/main/java/fr/ideeess/manager/PrisonersManager.java
@@ -1,9 +1,131 @@
 package fr.ideeess.manager;
 
 import fr.ideeess.main.JailSystem;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.*;
 
 public class PrisonersManager extends JailSystem {
 
+    public void createPrisoner(String playerName , int punition){
+        Player player = Bukkit.getPlayer(playerName);
+        PlayerInventory inventory = player.getInventory();
 
+        List<Map<String, Object>> itemsList = new ArrayList<>();
+        for (ItemStack item : inventory.getContents()) {
+            if (item != null) {
+                Map<String, Object> itemData = new HashMap<>();
+
+                // Durabilité
+                short durability = item.getDurability();
+                itemData.put("durability", durability);
+
+                // Enchantements
+                Map<String, Integer> enchantmentsMap = new HashMap<>();
+                Map<Enchantment, Integer> enchantments = item.getEnchantments();
+                for (Enchantment enchantment : enchantments.keySet()) {
+                    int level = enchantments.get(enchantment);
+                    String enchantmentName = enchantment.getKey().getKey();
+                    enchantmentsMap.put(enchantmentName, level);
+                }
+                itemData.put("enchantments", enchantmentsMap);
+
+                // Nom spécifique
+                ItemMeta meta = item.getItemMeta();
+                if (meta != null && meta.hasDisplayName()) {
+                    String displayName = meta.getDisplayName();
+                    itemData.put("display_name", displayName);
+                }
+
+                itemsList.add(itemData);
+            }
+        }
+
+        getConfig().set("prisonners."+playerName+".punition",punition);
+        getConfig().set("prisonners."+playerName+".inventory",itemsList);
+        saveConfig();
+
+    }
+
+    public void sendInJail(String playerName , int punition){
+        Player player = Bukkit.getPlayer(playerName);
+
+        assert player != null;
+        player.getInventory().clear();
+        player.teleport(JAILSPAWNLOCATION);
+        player.sendMessage(ChatColor.RED + "Vous êtes maintenant en prison .Vous devez casser " + punition + " blocs");
+
+        ItemStack pickaxe = new ItemStack(Material.STONE_PICKAXE); // Crée une pioche en pierre
+
+        // Ajoute l'enchantement "Unbreaking" de niveau maximal pour rendre la pioche incassable
+        pickaxe.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
+
+        // Définit l'attribut "generic.max_durability" de la pioche à une valeur élevée pour prolonger la durabilité
+        ItemMeta meta = pickaxe.getItemMeta();
+        AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(), "generic.max_durability", 100000, AttributeModifier.Operation.ADD_NUMBER);
+        meta.addAttributeModifier(Attribute.GENERIC_MAX_HEALTH, modifier);
+        pickaxe.setItemMeta(meta);
+
+    }
+
+    public void endOfJail(String playerName){
+        Player player = Bukkit.getPlayer(playerName);
+
+        player.teleport(JAILEXITLOCATION);
+        player.getInventory().clear();
+
+        List<Map<String, Object>> itemsList = (List<Map<String, Object>>) getConfig().get("prisonners."+playerName+".punition");
+        for (Map<String, Object> itemData : itemsList) {
+            ItemStack item = new ItemStack(Material.STONE); // Remplace le type d'objet par défaut par exemple
+
+            // Durabilité
+            if (itemData.containsKey("durability")) {
+                short durability = (short) itemData.get("durability");
+                item.setDurability(durability);
+            }
+
+            // Enchantements
+            if (itemData.containsKey("enchantments")) {
+                Map<String, Integer> enchantmentsMap = (Map<String, Integer>) itemData.get("enchantments");
+                for (Map.Entry<String, Integer> entry : enchantmentsMap.entrySet()) {
+                    String enchantmentName = entry.getKey();
+                    int level = entry.getValue();
+                    Enchantment enchantment = Enchantment.getByKey(NamespacedKey.minecraft(enchantmentName));
+                    if (enchantment != null) {
+                        item.addUnsafeEnchantment(enchantment, level);
+                    }
+                }
+            }
+
+            // Nom spécifique
+            if (itemData.containsKey("display_name")) {
+                String displayName = (String) itemData.get("display_name");
+                ItemMeta meta = item.getItemMeta();
+                if (meta != null) {
+                    meta.setDisplayName(displayName);
+                    item.setItemMeta(meta);
+                }
+            }
+
+            // Ajouter l'objet à l'inventaire du joueur
+            player.getInventory().addItem(item);
+        }
+
+        player.sendMessage(ChatColor.GREEN + "Vous venez d'être libéré , merci de ne pas recommencer.");
+
+        getConfig().set("prisonners."+playerName+".punition",null);
+        getConfig().set("prisonners."+playerName+".inventory",null);
+        saveConfig();
+    }
 
 }
